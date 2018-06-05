74fedd5be0e7d1b0892eb55928bdb5c21038a4ad
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 15:16:10 +0900

    all file

--- InitialFixation.php
+++ InitialFixation.php
@@ -5,4 +5,24 @@ require_once './config.php';
 use Solt9029\Utility;
 $utility = new Utility();
 
-var_dump($utility->getInitialFixation(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, './csv/test.csv'));
+$output_data = [];
+
+foreach (glob('./csv/*.csv') as $file) {
+  if (!strpos($file, 'eye')) {
+    continue;
+  }
+  $pathinfo = pathinfo($file);
+  $exploded_filename = explode('_', $pathinfo['filename']);
+  $initial_fixation = $utility->getInitialFixation(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, $file);
+  $output_data[] = [$exploded_filename[0], $exploded_filename[1], $initial_fixation['center_position'][0], $initial_fixation['center_position'][1]];
+}
+
+$output_file = './output/' . time() . '.csv';
+// touch($output_file);
+$file = fopen($output_file, 'w');
+if ($file) {
+  foreach ($output_data as $line) {
+    fputcsv($file, $line);
+  }
+}
+fclose($file);
