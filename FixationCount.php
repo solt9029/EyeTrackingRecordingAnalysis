f0e10f4906f327749a44e014c0568c2e3655613a
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 10:48:30 +0900

    多分できた

--- FixationCount.php
+++ FixationCount.php
@@ -1,4 +1,9 @@
 <?php
+require __DIR__ . "/vendor/autoload.php";
+use Solt9029\Utility;
+
+$utility = new Utility();
+
 define('DPI', 220.84); // 1インチ当たりのドット数
 define('PRECISION', 0.5); // 計測誤差（度）
 define('DISTANCE', 65); // 目からディスプレイまでの距離（cm）
@@ -13,44 +18,38 @@ $flick_range_cm = DISTANCE * tan(deg2rad(FLICK)); // 固視微動範囲（cm）
 $range_cm = $precision_error_range_cm + $flick_range_cm; // 計測誤差範囲+固視微動範囲（cm）
 $range_px = $range_cm * $dpc;
 
-$filename = './example.csv';
+$filename = './0_0_eye.csv';
 $file = fopen($filename, 'r');
 $recordings = [];
 while ($line = fgetcsv($file)) {
+  // 同時刻のものは1つだけ格納する
+  if (count($recordings) > 0) {
+    if ($recordings[count($recordings) - 1][2] === $line[2]) {
+      continue;
+    }
+  }
   $recordings[] = $line;
 }
 
+$fixations = [];
 
-
-// $fixations = [];
-
-// for ($i = 0; $i < count($recordings); $i++) {
-//   $initial_recording = $recordings[$i];
-//   while (true) {
-//     if (dist($initial_recording[0], $initial_recording[1], $recordings[$i][0], $recordings[$i][1]) > $range_px) {
-//       if ($recordings[$i - 1][2] - $initial_recording[2] > MIN_DURATION) {
-//         $initial_recording[3] = $recordings[$i - 1][2] - $initial_recording[2];
-//         $fixations[] = $initial_recording;
-//       }
-//       break;
-//     }
-
-//     // インクリメント
-//     $i++;
-//     if ($i >= count($recordings)) {
-//       break;
-//     }
-//   }
-// }
-
-// var_dump($fixations);
-
-// function dist($x1, $y1, $x2, $y2) {
-//   return sqrt(pow(($x2 - $x1), 2) + pow(($y2 - $y1), 2));
-// }
-
-function dist($positions) {
-  $x_dist = abs($positions[1][0] - $positions[0][0]);
-  $y_dist = abs($positions[1][1] - $positions[0][1]);
-  return sqrt(pow($x_dist, 2) + pow($y_dist, 2));
+for ($first_index = 0; $first_index < count($recordings); $first_index++) {
+  $recording_stack = [];
+  for ($second_index = $first_index; $second_index < count($recordings); $second_index++) {
+    $recording_stack[] = $recordings[$second_index];
+    $circle = $utility->getCircleContainingAllPositions($recording_stack);
+    // 注視範囲を超えていた場合、今までのスタックを全て注視として、クリア
+    if ($circle['radius'] > $range_px || $second_index + 1 >= count($recordings)) {
+      array_pop($recording_stack); // 最後のやつははみ出てるってことなので削除しよう
+      $duration = $recording_stack[count($recording_stack) - 1][2] - $recording_stack[0][2];
+      if ($duration > MIN_DURATION) {
+        $circle = $utility->getCircleContainingAllPositions($recording_stack);
+        $fixations[] = [$circle['center_position'][0], $circle['center_position'][1], $recording_stack[0][2], $recording_stack[count($recording_stack) - 1][2], $duration];
+        $first_index = $second_index;
+      }
+      break;
+    }
+  }
 }
+
+var_dump($fixations);
