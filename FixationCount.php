e762d62321f94144482aac2a7e7d557abe0a6492
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 12:26:10 +0900

    config

--- FixationCount.php
+++ FixationCount.php
@@ -1,24 +1,17 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
+require_once './config.php';
+
 use Solt9029\Utility;
-
 $utility = new Utility();
-
-define('DPI', 220.84); // 1インチ当たりのドット数
-define('PRECISION', 0.5); // 計測誤差（度）
-define('DISTANCE', 65); // 目からディスプレイまでの距離（cm）
-define('FLICK', 0.01); // 固視微動（度）
-define('MIN_DURATION', 100); // 注視したとする最低時間（ミリ秒） → いろいろな論があるっぽいので、とりあえず100ミリ秒で
-// define('HZ', 90); // 視線計測機のサンプリング頻度 → CSVファイルに時刻が入っているのでいらない
 
 $dpc = DPI / 2.54; // 1センチ当たりのドット数
 $precision_error_range_cm = DISTANCE * tan(deg2rad(PRECISION)); // 計測誤差範囲（cm）
 $flick_range_cm = DISTANCE * tan(deg2rad(FLICK)); // 固視微動範囲（cm）
-
 $range_cm = $precision_error_range_cm + $flick_range_cm; // 計測誤差範囲+固視微動範囲（cm）
 $range_px = $range_cm * $dpc;
 
-$filename = './test.csv';
+$filename = './csv/test.csv';
 $file = fopen($filename, 'r');
 $recordings = [];
 while ($line = fgetcsv($file)) {
