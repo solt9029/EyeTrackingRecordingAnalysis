2b3f7b0db96db4ad654286980fc5389ec496fdd9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 14:22:47 +0900

    fixation utility

--- InitialFixation.php
+++ InitialFixation.php
@@ -5,4 +5,4 @@ require_once './config.php';
 use Solt9029\Utility;
 $utility = new Utility();
 
-var_dump($utility->getFixationCount(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, './csv/test.csv'));
+var_dump($utility->getInitialFixation(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, './csv/test.csv'));
