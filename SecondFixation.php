e5036f01d1d8a68eea7003c2d04b7cdd5b2ddea2
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-06 13:09:14 +0900

    second

--- SecondFixation.php
+++ SecondFixation.php
@@ -12,12 +12,11 @@ foreach (glob('./input/*.csv') as $file) {
   }
   $pathinfo = pathinfo($file);
   $exploded_filename = explode('_', $pathinfo['filename']);
-  $initial_fixation = Utility::getInitialFixation(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, $file);
-  $output_data[] = [$exploded_filename[0], $exploded_filename[1], $initial_fixation['center_position'][0], $initial_fixation['center_position'][1]];
+  $second_fixation = Utility::getSecondFixation(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, $file);
+  $output_data[] = [$exploded_filename[0], $exploded_filename[1], $second_fixation['center_position'][0], $second_fixation['center_position'][1]];
 }
 
 $output_file = './output/' . time() . '.csv';
-// touch($output_file);
 $file = fopen($output_file, 'w');
 if ($file) {
   foreach ($output_data as $line) {
