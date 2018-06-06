ac81e4a8885287d31142ffe63c4c99b77a6e9136
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-06 16:32:52 +0900

    fixation count

--- FixationCount.php
+++ FixationCount.php
@@ -4,4 +4,24 @@ require_once './config.php';
 
 use Solt9029\Utility;
 
-var_dump(Utility::getFixationCount(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, './csv/test.csv'));
+$output_data = [];
+
+foreach (glob('./input/*.csv') as $file) {
+  if (!strpos($file, 'eye')) {
+    continue;
+  }
+  $pathinfo = pathinfo($file);
+  $exploded_filename = explode('_', $pathinfo['filename']);
+  $fixation_count = Utility::getFixationCount(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, $file);
+  $output_data[] = [$exploded_filename[0], $exploded_filename[1], $fixation_count];
+}
+
+$output_file = './output/' . time() . '.csv';
+// touch($output_file);
+$file = fopen($output_file, 'w');
+if ($file) {
+  foreach ($output_data as $line) {
+    fputcsv($file, $line);
+  }
+}
+fclose($file);
