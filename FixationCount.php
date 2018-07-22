40e72a463a5d81b6d124547cf87e7f40807ebd4e
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-07-22 16:37:16 +0900

    update FixationCount.php and InitialFixation.php and SecondFixation.php (they have to be used after Fixations.php)

--- FixationCount.php
+++ FixationCount.php
@@ -1,23 +1,32 @@
 <?php
+// Fixations.php後のファイル群に対して実行するよ！
+// 例： php FixationCount.php output/1532242523/20180717113505_tasuku/16
+
 require __DIR__ . "/vendor/autoload.php";
 require_once './config.php';
 
 use Solt9029\Utility;
 
+if (count($argv) < 2) {
+  return;
+}
+$target_dir = $argv[1];
+$filelist = Utility::getFileList($target_dir);
 $output_data = [];
 
-foreach (glob('./input/*.csv') as $file) {
+foreach ($filelist as $file) {
   if (!strpos($file, 'eye')) {
     continue;
   }
+
   $pathinfo = pathinfo($file);
   $exploded_filename = explode('_', $pathinfo['filename']);
-  $fixation_count = Utility::getFixationCount(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, $file);
+  $fixation_count = count(file($file));
   $output_data[] = [$exploded_filename[0], $exploded_filename[1], $fixation_count];
 }
 
-$output_file = './output/' . time() . '.csv';
-// touch($output_file);
+$output_file = $target_dir . '/' . 'FixationCount.csv';
+touch($output_file);
 $file = fopen($output_file, 'w');
 if ($file) {
   foreach ($output_data as $line) {
--- InitialFixation.php
+++ InitialFixation.php
@@ -1,23 +1,34 @@
 <?php
+// Fixations.php後のファイル群に対して実行するよ！
+// 例： php InitialFixation.php output/1532242523/20180717113505_tasuku/16
+
 require __DIR__ . "/vendor/autoload.php";
 require_once './config.php';
 
 use Solt9029\Utility;
 
+if (count($argv) < 2) {
+  return;
+}
+$target_dir = $argv[1];
+$filelist = Utility::getFileList($target_dir);
 $output_data = [];
 
-foreach (glob('./input/*.csv') as $file) {
-  if (!strpos($file, 'eye')) {
+foreach ($filelist as $filename) {
+  if (!strpos($filename, 'eye')) {
     continue;
   }
-  $pathinfo = pathinfo($file);
+
+  $file = fopen($filename, 'r');
+  $initial_fixation = fgetcsv($file);
+
+  $pathinfo = pathinfo($filename);
   $exploded_filename = explode('_', $pathinfo['filename']);
-  $initial_fixation = Utility::getInitialFixation(DPI, PRECISION, DISTANCE, FLICK, MIN_DURATION, $file);
-  $output_data[] = [$exploded_filename[0], $exploded_filename[1], $initial_fixation['center_position'][0], $initial_fixation['center_position'][1]];
+  $output_data[] = [$exploded_filename[0], $exploded_filename[1], $initial_fixation[0], $initial_fixation[1]];
 }
 
-$output_file = './output/' . time() . '.csv';
-// touch($output_file);
+$output_file = $target_dir . '/' . 'InitialFixation.csv';
+touch($output_file);
 $file = fopen($output_file, 'w');
 if ($file) {
   foreach ($output_data as $line) {
